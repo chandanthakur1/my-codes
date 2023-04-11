82ba85ee3ed1a67548a5583f9b32a8de65f4ab7b
Chandan Thakur <chandan.thakur@go-yubi.com>
2023-04-11 14:59:58 +0530

    All Codes

--- flight_details/config/environments/development.rb
+++ flight_details/config/environments/development.rb
@@ -65,11 +65,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host:'localhost', port: 3000}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :domain => "mail.google.com",
-    :user_name => 'thakurchandan2640@gmail.com',
-    :password => "Nirmala@2640",
+    :address => '',
+    :port => ,
+    :domain => "",
+    :user_name => '',
+    :password => "",
     :authentication => 'plain',
     :enable_starttls_auto => true
   }
